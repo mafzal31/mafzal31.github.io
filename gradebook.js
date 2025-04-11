2a9b5800dea9acf50431d57dd949b8c3c6da0718
mafzal31 <mafzal@my.waketech.edu>
2025-04-10 22:08:46 -0400

    I have added the gradebook HTML and JavaScript files

---
+++ gradebook.js
@@ -0,0 +1,11 @@
+function fetchGradeData() {
+  console.log("fetchGradeData called - will fetch data from PostgreSQL DB in future.");
+}
+
+function populateGradebook() {
+  console.log("populateGradebook called - will populate the table with data.");
+}
+
+// Call the functions to test
+fetchGradeData();
+populateGradebook();
